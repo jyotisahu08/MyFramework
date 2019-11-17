a28b7d03999bbd976eb5dbd28b4dba70241842d1
Jyoti <Jyoti@JYOTI-PC>
2019-11-17 20:46:41 +0530

    my first commit

---
+++ src/main/java/MyFrstMavnProjct/MavenProjct/App.java
@@ -0,0 +1,13 @@
+package MyFrstMavnProjct.MavenProjct;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/MyFrstMavnProjct/MavenProjct/AppTest.java
@@ -0,0 +1,12 @@
+package MyFrstMavnProjct.MavenProjct;
+
+import org.testng.annotations.Test;
+
+public class AppTest{
+	@Test
+	public void custTest()
+	{
+		System.out.println("My test");
+	}
+	
+}
